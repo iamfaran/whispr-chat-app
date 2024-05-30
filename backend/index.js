49453c5573f8b16ac7f48792c95d722f305503da
Faran Javed <faran1997@outlook.com>
2024-05-30 14:09:24 +0500

    setup project

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// load env variables from .env file
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
