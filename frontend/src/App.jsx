edc99303cf6176e393c50ce79e9e2807980a349d
Faran Javed <faran1997@outlook.com>
2024-06-04 20:00:57 +0500

    setup react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="p-4  h-screen flex items-center justify-center">
-      {/* <Login />
-      <Register /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
