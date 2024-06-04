fbd5ea11693d046d0a19878df33b0440e52a2137
Faran Javed <faran1997@outlook.com>
2024-06-04 16:37:06 +0500

    create sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="p-4  h-screen flex items-center justify-center">
-      <Login />
-      <Register />
+      {/* <Login />
+      <Register /> */}
+      <Home />
     </div>
   );
 }
