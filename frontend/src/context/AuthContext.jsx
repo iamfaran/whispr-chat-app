166eea4c230981b7ed6d2c97be73e0a7d36681e0
Faran Javed <faran1997@outlook.com>
2024-06-05 21:28:30 +0500

    add auth context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import jwt from "jsonwebtoken";
+
+// Create the AuthContext
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+});
+
+// Create the AuthProvider component
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    // Check for existing token in local storage and decode it
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwt.decode(token);
+      if (decodedToken.exp * 1000 > Date.now()) {
+        setUser(decodedToken);
+      } else {
+        localStorage.removeItem("token");
+      }
+    }
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setUser(jwt.decode(token));
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  const value = { user, login, logout };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+// Create a custom hook to access the AuthContext value
+function useAuth() {
+  return useContext(AuthContext);
+}
+
+export { AuthProvider, useAuth };
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
