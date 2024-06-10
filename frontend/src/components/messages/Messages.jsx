40033f9ea9af25250e4c0caa0bc7623a6bca3960
Faran Javed <faran1997@outlook.com>
2024-06-10 19:19:47 +0500

    console log message for conversations

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,5 +1,8 @@
 import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  console.log("Messages: ", messages, "Loading: ", loading);
   return (
     <div className="px-4 flex-1 overflow-auto">
       <Message />
---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useConversationStore } from "../zustand/useConversationStore";
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } =
+    useConversationStore();
+
+  useEffect(() => {
+    console.log("use effect ran");
+    const id = selectedConversation?._id;
+    const getMessages = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(`/api/messages/${id}`);
+        setMessages(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMessages();
+  }, [setMessages, selectedConversation?._id]);
+
+  return { messages, loading };
+};
+
+export default useGetMessages;
