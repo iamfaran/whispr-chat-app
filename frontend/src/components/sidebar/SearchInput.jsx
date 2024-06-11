75a2ebdba992f2eb0093cc471c287d589f99498b
Faran Javed <faran1997@outlook.com>
2024-06-11 12:58:53 +0500

    add search filter

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,11 +1,29 @@
 import { IoSearchSharp } from "react-icons/io5";
+import { useForm } from "react-hook-form"; // Import useForm
+import { useConversationStore } from "../../zustand/useConversationStore";
+import useGetConversations from "../../hooks/useGetConversations";
+
 const SearchInput = () => {
+  const { register, handleSubmit } = useForm();
+  const { conversations } = useGetConversations();
+  const { setSelectedConversation } = useConversationStore();
+
+  const onSubmit = (data) => {
+    const searchQuery = data.search; // Access the search query from data
+    setSelectedConversation(
+      conversations.find((conversation) =>
+        conversation.fullName.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit(onSubmit)}>
       <input
         type="text"
         placeholder="Search…"
         className="input input-bordered rounded-full"
+        {...register("search")} // Register the input
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
@@ -13,4 +31,5 @@ const SearchInput = () => {
     </form>
   );
 };
+
 export default SearchInput;
