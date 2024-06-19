bc5abb3612594cb92e5cbc3054492d9144b8088a
Faran Javed <faran1997@outlook.com>
2024-06-20 00:36:51 +0500

    fix welcome user name

--- frontend/src/components/messages/NoChat.jsx
+++ frontend/src/components/messages/NoChat.jsx
@@ -1,8 +1,10 @@
+import { useAuth } from "../../context/AuthContext";
 const NoChat = () => {
+  const { user } = useAuth();
   return (
     <div className="sm:flex items-center justify-center w-full h-full hidden ">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 Dummy ❄</p>
+        <p>Welcome 👋 {user ? user.fullName : "User"} ❄</p>
         <p>Select a chat to start messaging</p>
       </div>
     </div>
